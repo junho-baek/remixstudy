ac7df20e13141efcc12881a092744bc89bfa229a
junho-baek <junho6610@yonsei.ac.kr>
2025-02-13 03:36:44 +0900

    2.0

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/common/components/ui/button"; // ~는 프로젝트 루트를 의미한다.
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "~/common/components/ui/navigation-menu";
+
+import { Switch } from "~/common/components/ui/switch";
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <div>Home</div>
+
+      <div className="flex justify-center items-center h-screen">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink href="/" className="w-56 block h-56">
+                  adsfasdfasdf
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <Switch
+          onClick={() => document.documentElement.classList.toggle("dark")}
+        />
+
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
 }
